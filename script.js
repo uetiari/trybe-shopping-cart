d119cbc2ff99d5f5c164d997712a4a03db252ce5
Ariane Ueti <arianeueti@gmail.com>
2021-08-16 18:18:51 -0300

    Correção de problemas no NPM

--- script.js
+++ script.js
@@ -27,6 +27,24 @@ function createProductItemElement({ sku, name, image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+async function getProductsApi(product) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  await response.json()
+    .then((data) => data.results
+    .forEach((obj) => {
+      const product = {
+        sku: obj.id,
+        name: obj.title,
+        image: obj.thumbnail,
+      };
+      const ol = document.querySelector('.items');
+      ol.appendChild(createProductItemElement(product));
+    }));
+  // const ol = document.querySelector('.items');
+  // ol.appendChild(createProductItemElement(product));
+}
+// consultei o github da Beatriz Ribeiro porque meus produtos não estavam aparecendo, mas olhando o que ela fez, percebi que minhas linhas 41 e 42 estavam fora do escopo e por isso estava undefined.
+// https://github.com/tryber/sd-014-b-project-shopping-cart/pull/52/
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
@@ -40,4 +58,6 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+window.onload = () => {
+  getProductsApi('computador');
+};
