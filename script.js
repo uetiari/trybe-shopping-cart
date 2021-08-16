0569175e707cc0b66dc83a8ad6c35f260a480113
Ariane Ueti <arianeueti@gmail.com>
2021-08-16 16:44:53 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -27,6 +27,25 @@ function createProductItemElement({ sku, name, image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+
+async function getProductsApi(product) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  const json = await response.json()
+    .then((data) => data.results
+    .forEach((obj) => {
+      const product = {
+        sku: obj.id,
+        name: obj.title,
+        image: obj.thumbnail,
+      };
+      const ol = document.querySelector('.items');
+      ol.appendChild(createProductItemElement(product));
+    }));
+  // const ol = document.querySelector('.items');
+  // ol.appendChild(createProductItemElement(product));
+}
+// consultei o github da Beatriz Ribeiro porque meus produtos não estavam aparecendo, mas olhando o que ela fez, percebi que minhas linhas 41 e 42 estavam fora do escopo e por isso estava undefined.
+// https://github.com/tryber/sd-014-b-project-shopping-cart/pull/52/
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
@@ -40,4 +59,6 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+window.onload = () => {
+  getProductsApi('computador');
+};
