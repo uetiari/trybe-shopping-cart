086d8d90bc9aa5babdcc0848853ca84191fbae83
Ariane Ueti <arianeueti@gmail.com>
2021-08-16 20:23:52 -0300

    Tentando pela miléssima vez arrumar o EsLint

--- script.js
+++ script.js
@@ -24,15 +24,14 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-async function getProductsApi(product) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+async function getProductsApi(prod) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${prod}`);
   await response.json()
-    .then((data) => data.results
-    .forEach((obj) => {
+    .then((data) => data.results.forEach((obj) => {
       const product = {
         sku: obj.id,
         name: obj.title,
@@ -40,24 +39,26 @@ async function getProductsApi(product) {
       };
       const ol = document.querySelector('.items');
       ol.appendChild(createProductItemElement(product));
+      console.log(product);
     }));
   // const ol = document.querySelector('.items');
   // ol.appendChild(createProductItemElement(product));
+  return response;
 }
 // consultei o github da Beatriz Ribeiro porque meus produtos não estavam aparecendo, mas olhando o que ela fez, percebi que minhas linhas 41 e 42 estavam fora do escopo e por isso estava undefined.
 // https://github.com/tryber/sd-014-b-project-shopping-cart/pull/52/
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 window.onload = () => {
   getProductsApi('computador');
